52fc0136875d37102d9cbf092671f3632254f469
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2024-01-12 19:46:39 +0100

    Refactor: Extract calculation methods

--- src/core/bowlingGame.ts
+++ src/core/bowlingGame.ts
@@ -8,27 +8,27 @@ export class BowlingGame {
 	}
     
     calculateFinalScore() {        
-        let finalScore = 0;
-        
-        //Standart points
-        this.rolls.forEach(element => {
-            finalScore += element
-        });
+        return this.calculateTotalBasePoints() + this.calculateTotalSpecialPoints();
+    }
 
-        //Frames
-        for (let cntFrame = 0; cntFrame < this.rolls.length/2; cntFrame += 2) {
+    private calculateTotalSpecialPoints() {
+        let totalSpecialPoints = 0;
+        for (let cntFrame = 0; cntFrame < this.rolls.length / 2; cntFrame += 2) {
             let frameSubtotal = this.rolls[cntFrame] + this.rolls[cntFrame + 1];
 
-            if(this.isStrike(frameSubtotal, cntFrame)){
-                finalScore += this.rolls[cntFrame + 2];
-                finalScore += this.rolls[cntFrame + 3];
+            if (this.isStrike(frameSubtotal, cntFrame)) {
+                totalSpecialPoints += this.rolls[cntFrame + 2];
+                totalSpecialPoints += this.rolls[cntFrame + 3];
             }
-            else if(this.isSpare(frameSubtotal)){
-                finalScore += this.rolls[cntFrame + 2];
+            else if (this.isSpare(frameSubtotal)) {
+                totalSpecialPoints += this.rolls[cntFrame + 2];
             }
         }
-        
-        return finalScore;
+        return totalSpecialPoints;
+    }
+
+    private calculateTotalBasePoints() {
+        return this.rolls.reduce((total, pins) => total + pins, 0);
     }
 
     private isSpare(frameSubtotal: number) {
