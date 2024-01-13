83de3e2d851d7902556a456077175f2caee269b9
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2024-01-13 20:00:36 +0100

    Refactor: Include new tests

--- src/core/release.ts
+++ src/core/release.ts
@@ -1,8 +1,16 @@
+const EMPTY = -1;
+const MIN_NUM_PINS = 0;
+const MAX_NUM_PINS = 10;
+
 export class Release {
     private _pins: number;
+    private _empty: boolean = false;
 
     private constructor(pins: number) {
         this._pins = pins;
+        if(this._pins === EMPTY) {
+            this._empty = true;
+        }
     }
 
     // Getter para obtener el número de bolos derribados
@@ -10,13 +18,21 @@ export class Release {
         return this._pins;
     }
 
+    isEmpty(): boolean { 
+        return this._empty; 
+    }
+
     // Método de factoría estático con validación del rango
     static create(pins: number): Release {
         // Verificar que el número de bolos esté en el rango permitido (0 a 10)
-        if (pins < 0 || pins > 10) {
+        if (pins < MIN_NUM_PINS || pins > MAX_NUM_PINS) {
             throw new Error("El número de bolos derribados debe estar entre 0 y 10.");
         }
 
         return new Release(pins);
+    }
+
+    static createEmpty(): Release {
+        return new Release(EMPTY);
     }
 }
--- src/tests/release.test.ts
+++ src/tests/release.test.ts
@@ -2,6 +2,15 @@ import { Release } from '../core/release';
 
 describe('Release', ()=>{
 
+    const EMPTY = -1;
+    
+    it('should accept an empty release in case a strike', ()=>{
+        let emptyRelease = Release.createEmpty();
+        let pins = emptyRelease.pins;
+        expect(pins).toBe(EMPTY);
+        expect(emptyRelease.isEmpty()).toBe(true);
+    })
+    
     it('should accept a number of pins when created', ()=>{
         let release = Release.create(1);
         let pins = release.pins;
