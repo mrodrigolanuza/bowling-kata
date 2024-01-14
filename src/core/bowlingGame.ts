7d83d55b6c7ddc5229f2ae2eaca62b2dbfb16080
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2024-01-14 11:51:54 +0100

    Refactor: Extract methods and rename variables in calculateFinalScore method

--- src/core/bowlingGame.ts
+++ src/core/bowlingGame.ts
@@ -2,6 +2,7 @@ import { Frame } from "./frame";
 import { Release } from "./release";
 
 const STRIKE_NUM_PINS = 10;
+const MIN_GAME_FRAMES = 10;
 
 export class BowlingGame {
     rolls: number[] = [];
@@ -14,46 +15,65 @@ export class BowlingGame {
 	}
     
     calculateFinalScore() {        
-        
-        //Create frames grouping rolls
-        for (let idRoll = 0; idRoll < this.rolls.length; idRoll++){
-            //Strike
-            if(this.rolls[idRoll] == STRIKE_NUM_PINS){
-                this.frames.push(Frame.create(Release.create(STRIKE_NUM_PINS), 
-                                              Release.createEmpty()));
-                continue;
-            }
-            //Other case
-            this.frames.push(Frame.create(Release.create(this.rolls[idRoll]), 
-                                          Release.create(this.rolls[idRoll+1])));;
-            idRoll++;
-        }
+        this.createFramesGroupingRolls();
+        return this.calculateFramesFinalScore();
+    }
 
-        //Calculate game points
-        let totalGamePoints: number = 0; 
-        for (let idFrame = 0; idFrame < 10; idFrame++){
+    private calculateFramesFinalScore() {
+        let framesFinalScore: number = 0;
+        for (let idFrame = 0; idFrame < MIN_GAME_FRAMES; idFrame++) {
 
             let frame: Frame = this.frames[idFrame];
-            totalGamePoints += frame.getTotalPoints()
+            framesFinalScore += frame.getTotalPoints();
 
-            if (frame.isStrike()){
+            if (frame.isStrike()) {
                 let nextOneFrame: Frame = this.frames[idFrame + 1];
-                if (nextOneFrame.isStrike()){
-                    totalGamePoints += nextOneFrame.getFirstReleasePoints();
+                if (nextOneFrame.isStrike()) {
+                    framesFinalScore += nextOneFrame.getFirstReleasePoints();
                     let nextSecondOneFrame: Frame = this.frames[idFrame + 2];
-                    totalGamePoints += nextSecondOneFrame.getFirstReleasePoints();
+                    framesFinalScore += nextSecondOneFrame.getFirstReleasePoints();
                 }
-                else{
-                    totalGamePoints += nextOneFrame.getTotalPoints();
+                else {
+                    framesFinalScore += nextOneFrame.getTotalPoints();
                 }
             }
 
-            if (frame.isSpire()){
+            if (frame.isSpire()) {
                 let nextFrame: Frame = this.frames[idFrame + 1];
-                totalGamePoints += nextFrame.getFirstReleasePoints();
+                framesFinalScore += nextFrame.getFirstReleasePoints();
             }
         }
+        return framesFinalScore;
+    }
 
-        return totalGamePoints;
+    private createFramesGroupingRolls() {
+        for (let idRoll = 0; idRoll < this.rolls.length; idRoll++) {
+            
+            if (this.isStrikeRoll(idRoll)) {
+                this.frames.push(this.createStrikeFrame());
+                continue;
+            }
+
+            this.frames.push(this.createStandartTwoRollFrame(idRoll));;
+            idRoll = increaseIdRoll_StandartFrameNeedsTwoReleases(idRoll);
+        }
+
+        function increaseIdRoll_StandartFrameNeedsTwoReleases(idRoll: number) {
+            return ++idRoll;
+        }
+    }
+
+    private createStrikeFrame(): Frame {
+        return Frame.create(Release.create(STRIKE_NUM_PINS),
+            Release.createEmpty());
+    }
+
+    private createStandartTwoRollFrame(idRoll: number): Frame {
+        return Frame.create(Release.create(this.rolls[idRoll]),
+            Release.create(this.rolls[idRoll + 1]));
+    }
+
+    private isStrikeRoll(idRoll: number) {
+        return this.rolls[idRoll] == STRIKE_NUM_PINS;
     }
 }
